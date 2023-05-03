2d52421fd442607ceefd4bef9bb59a8dfe78031c
kevmeloo <kevenmelodn@gmail.com>
2023-05-03 13:23:42 -0300

    alteracao no nome do h1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Página inicial!!</h1>');
+  res.send('<h1>Página INICIAL!</h1>');
 });
 
 app.get('/sobre', (req, res) => {
