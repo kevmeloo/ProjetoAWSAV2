acfacfbe425e9a01e021688db7303886c923f467
Keven Melo <115496525+kevmeloo@users.noreply.github.com>
2023-05-03 11:43:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Página inicial</h1>');
+});
+
+app.get('/sobre', (req, res) => {
+  res.send('<h1>Sobre nós</h1>');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('<h1>Contato</h1>');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
