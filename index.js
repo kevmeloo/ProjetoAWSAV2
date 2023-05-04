ef93d56a29def2fddadc8322f4ff8cc95957ef3b
kevmeloo <kevenmelodn@gmail.com>
2023-05-04 15:22:49 -0300

    adicao dos comentarios

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Importação do Express 
+const app = express(); //Criação de um aplicativo Express.js chamado 'app'
 
 app.get('/', (req, res) => {
-  res.send('<h1>Página Inicial!</h1>');
-});
+  res.send('<h1>Página Inicial!</h1>'); //Definição das rotas 
+}); // res.send servidor enviando resposta ao cliente
 
 app.get('/sobre', (req, res) => {
   res.send('<h1>Sobre nós</h1>');
@@ -13,7 +13,7 @@ app.get('/contato', (req, res) => {
   res.send('<h1>Contato</h1>');
 });
 
-const PORT = 3000;
+const PORT = 3000; // Configuração do servidor para escurtar na porta 3000
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
